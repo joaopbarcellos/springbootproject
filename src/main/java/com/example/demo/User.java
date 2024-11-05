7eb78f25e7792b7d77229839d0361d5c8440a9d7
joaopbarcellos <jpzbarcellos@gmail.com>
2024-11-05 13:37:09 -0300

    controller and user classes

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,5 @@
+package com.example.demo;
+
+public class User {
+    protected String login, nome, senha;
+}
--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -3,18 +3,29 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class UserController {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(UserController.class, args);
 	}
 
 	@GetMapping(value = "/")
 	public String hello() {
 		return "Hello World";
+	}
+
+	@PostMapping(value = "/post")
+	public String post(){
+		return "Post";
+	}
+
+	@GetMapping(value = "/teste")
+	public String Teste() {
+		return "Teste";
 	}
 }
