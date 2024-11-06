021fb75c193aca09a3c7dab2823f248d1b9a7b48
joaopbarcellos <jpzbarcellos@gmail.com>
2024-11-06 10:59:23 -0300

    new routes

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ WORKDIR /app
 
 # Copiando o arquivo .xml e os arquivos Java para o diretorio de trabalho
 COPY pom.xml .
+RUN mvn dependency:go-offline
 COPY src ./src
 
 # Expondo a porta 8080
--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,38 +1,56 @@
 package com.example.demo;
 
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @SpringBootApplication
+@RequestMapping("/api/UserService/users")
 public class UserController {
+
+	public JSONObject users = new JSONObject();
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserController.class, args);
 	}
 
-	@GetMapping("/")
-	public String hello() {
-		return "Hello World";
+	@PostMapping()
+	public String postUser(@RequestBody User user){
+		if (this.users.has(Long.toString(user.getId()))){
+			return "Erro: usuário já cadastrado.";
+		}
+		JSONObject user_json = new JSONObject();
+		user_json.put("nome", user.getNome());
+		user_json.put("login", user.getLogin());
+		this.users.put(Long.toString(user.getId()), user_json);
+		return "Usuário cadastrado com sucesso.";
 	}
 
-	@PostMapping("/UserManagement/rest/UserService/users")
-	public String post(@RequestBody User user){
-		return "Post: " + user.getId();
+	@GetMapping()
+	public String getUsers() {
+		return this.users.toString();			
 	}
 
-	@GetMapping("/UserManagement/rest/UserService/users")
-	public String Teste() {
-		return "Teste";
-	}
-
-	@GetMapping("/UserManagement/rest/UserService/users/{id}")
+	@GetMapping("/{id}")
 	public String getUserByID(@PathVariable("id") Long id) {
-		return "Teste";
+		JSONObject user = this.users.getJSONObject(Long.toString(id));
+		return user.toString();
+	}
+
+	@PutMapping("/{id}")
+	public String putUser(@PathVariable("id") Long id, @RequestBody User user) {
+		JSONObject user_json = new JSONObject();
+		user_json.put("nome", user.getNome());
+		user_json.put("login", user.getLogin());
+		this.users.put(Long.toString(id), user_json);
+		return "Usuário atualizado com sucesso.";
+	}
+
+	@DeleteMapping("/{id}")
+	public String deleteUser(@PathVariable("id") Long id) {
+		this.users.remove(Long.toString(id));
+		return "Usuário removido com sucesso.";
 	}
 }
